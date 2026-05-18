3ff4964e468f4f0255fa311622e5b58aa99f7609
erik <erik@erik.xyz>
2026-05-18 14:12:13 +0800

    feat: init window_manager with min size and centered startup

--- apps/flutter/lib/main.dart
+++ apps/flutter/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+  await windowManager.setMinimumSize(const Size(680, 480));
+  await windowManager.setSize(const Size(1280, 800));
+  await windowManager.center();
+
   runApp(const ProviderScope(child: AdsApp()));
 }
