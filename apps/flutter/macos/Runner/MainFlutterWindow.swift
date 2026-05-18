43188bb378461ce2f6b717c0f45542ffbe91a412
erik <erik@erik.xyz>
2026-05-18 14:18:18 +0800

    feat: hide native title bars on macOS/Windows/Linux for custom TitleBar

--- apps/flutter/macos/Runner/MainFlutterWindow.swift
+++ apps/flutter/macos/Runner/MainFlutterWindow.swift
@@ -3,13 +3,15 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
+    super.awakeFromNib()
+    self.titleVisibility = .hidden
+    self.titlebarAppearsTransparent = true
+    self.styleMask.insert(.fullSizeContentView)
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
   }
 }
