adf7fdf9ec8aeb38fabfc27fcc1dac2220196194
erik <erik@erik.xyz>
2026-05-18 14:13:32 +0800

    feat: tune theme for desktop (scaffold bg, divider config)

--- apps/flutter/lib/theme.dart
+++ apps/flutter/lib/theme.dart
@@ -6,5 +6,11 @@ class AppTheme {
     colorSchemeSeed: Colors.blue,
     brightness: Brightness.light,
     fontFamily: 'Roboto',
+    scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+    dividerTheme: const DividerThemeData(
+      space: 0,
+      thickness: 1,
+      color: Color(0xFFE0E0E0),
+    ),
   );
 }
